9c4b794199a9b4bb4dd99b2f991149dc5126ca6a
Pires2412 <seu-email@exemplo.com>
2026-02-13 11:24:34 -0300

    Inserção do mapa

--- static/js/features.js
+++ static/js/features.js
@@ -20,6 +20,12 @@ const boxDropdown = document.querySelectorAll('.box-dropdown')
 boxDropdown.forEach(el => {
     el.addEventListener('click', ()=>{
         const textBox = el.querySelector('.box-dropdown-text')
+        const elText = el.querySelector('.dropdown-title-icon')
     textBox.classList.toggle('is_open')
+    if (textBox.className == 'box-dropdown-text is_open') {
+        elText.textContent = '-'
+    } else {
+        elText.textContent = '+'
+    }
     })
 })
